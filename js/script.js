61ee6f03508c4f5671551c1fa48764d2cce87569
unknown <pdolecki.it@gmail.com>
2025-09-06 09:45:45 +0200

    Created collision physics

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ window.addEventListener("load", function () {
       this.speedY = 0;
       this.dx = 0;
       this.dy = 0;
-      this.speedModifier = 20;
+      this.speedModifier = 5;
     }
 
     draw(context) {
@@ -55,8 +55,13 @@ window.addEventListener("load", function () {
       this.collisionX += this.speedX * this.speedModifier;
       this.collisionY += this.speedY * this.speedModifier;
       this.game.obstacles.forEach((obstacle) => {
-        if (this.game.checkCollision(this, obstacle)) {
-          console.log("collision");
+        let [collision, distance, sumOfRadii, dx, dy] =
+          this.game.checkCollision(this, obstacle);
+        if (collision) {
+          const unit_x = dx / distance;
+          const unit_y = dy / distance;
+          this.collisionX = obstacle.collisionX + (sumOfRadii + 1) * unit_x;
+          this.collisionY = obstacle.collisionY + (sumOfRadii + 1) * unit_y;
         }
       });
     }
@@ -141,6 +146,7 @@ window.addEventListener("load", function () {
     }
 
     render(context) {
+      this.obstacles.forEach((obstacle) => obstacle.draw(context));
       this.player.draw(context);
       this.player.update();
       this.obstacles.forEach((obstacle) => obstacle.draw(context));
@@ -151,7 +157,7 @@ window.addEventListener("load", function () {
       const dy = a.collisionY - b.collisionY;
       const distance = Math.hypot(dy, dx);
       const sumOfRadii = a.collisionRadius + b.collisionRadius;
-      return distance < sumOfRadii;
+      return [distance < sumOfRadii, distance, sumOfRadii, dx, dy];
     }
 
     init() {
