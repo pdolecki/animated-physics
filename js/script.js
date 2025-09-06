ce81db2255074596d79c785df6706b519c8492e7
unknown <pdolecki.it@gmail.com>
2025-09-06 09:33:53 +0200

    Added simple collision detection logic

--- js/script.js
+++ js/script.js
@@ -54,6 +54,11 @@ window.addEventListener("load", function () {
       }
       this.collisionX += this.speedX * this.speedModifier;
       this.collisionY += this.speedY * this.speedModifier;
+      this.game.obstacles.forEach((obstacle) => {
+        if (this.game.checkCollision(this, obstacle)) {
+          console.log("collision");
+        }
+      });
     }
   }
 
@@ -139,6 +144,14 @@ window.addEventListener("load", function () {
       this.player.draw(context);
       this.player.update();
       this.obstacles.forEach((obstacle) => obstacle.draw(context));
+    }
+
+    checkCollision(a, b) {
+      const dx = a.collisionX - b.collisionX;
+      const dy = a.collisionY - b.collisionY;
+      const distance = Math.hypot(dy, dx);
+      const sumOfRadii = a.collisionRadius + b.collisionRadius;
+      return distance < sumOfRadii;
     }
 
     init() {
