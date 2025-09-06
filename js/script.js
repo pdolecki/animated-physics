4888ed873af2e07dba9259b2ba3b0c398efaa1e9
unknown <pdolecki.it@gmail.com>
2025-09-06 08:45:21 +0200

    Created initial obstacles rendering

--- js/script.js
+++ js/script.js
@@ -57,12 +57,39 @@ window.addEventListener("load", function () {
     }
   }
 
+  class Obstacle {
+    constructor(game) {
+      this.game = game;
+      this.collisionX = Math.random() * this.game.width;
+      this.collisionY = Math.random() * this.game.height;
+      this.collisionRadius = 100;
+    }
+
+    draw(context) {
+      context.beginPath();
+      context.arc(
+        this.collisionX,
+        this.collisionY,
+        this.collisionRadius,
+        0,
+        Math.PI * 2
+      );
+      context.save();
+      context.globalAlpha = 0.5;
+      context.fill();
+      context.restore();
+      context.stroke();
+    }
+  }
+
   class Game {
     constructor(canvas) {
       this.canvas = canvas;
       this.width = this.canvas.width;
       this.height = this.canvas.height;
       this.player = new Player(this);
+      this.numberOfObstacles = 5;
+      this.obstacles = [];
       this.mouse = {
         x: this.width * 0.5,
         y: this.height * 0.5,
@@ -90,10 +117,18 @@ window.addEventListener("load", function () {
     render(context) {
       this.player.draw(context);
       this.player.update();
+      this.obstacles.forEach((obstacle) => obstacle.draw(context));
+    }
+
+    init() {
+      for (let i = 0; i < this.numberOfObstacles; i++) {
+        this.obstacles.push(new Obstacle(this));
+      }
     }
   }
 
   const game = new Game(canvas);
+  game.init();
 
   function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
