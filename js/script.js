19f4429709c64ad230ad39279249e378ea3e686a
unknown <pdolecki.it@gmail.com>
2025-09-08 10:14:22 +0200

    Added Larva class

--- js/script.js
+++ js/script.js
@@ -206,7 +206,7 @@ window.addEventListener("load", function () {
       let collisionObjects = [
         this.game.player,
         ...this.game.obstacles,
-        ...this.enemies,
+        ...this.game.enemies,
       ];
       collisionObjects.forEach((object) => {
         let [collision, distance, sumOfRadii, dx, dy] =
@@ -218,6 +218,33 @@ window.addEventListener("load", function () {
           this.collisionY = object.collisionY + (sumOfRadii + 1) * unit_y;
         }
       });
+    }
+  }
+
+  class Larva {
+    constructor(game, x, y) {
+      this.game = game;
+      this.collisionX = x;
+      this.collisionY = y;
+      this.collisionRadius = 30;
+      this.image = document.getElementById("larva");
+      this.spriteWidth = 150;
+      this.spriteHeight = 150;
+      this.width = this.spriteWidth;
+      this.height = this.spriteHeight;
+      this.spriteX;
+      this.spriteY;
+      this.speedY = 1 + Math.random();
+    }
+
+    draw(context) {
+      context.drawImage(this.image, this.spriteX, this.spriteY);
+    }
+
+    update() {
+      this.collisionY -= this.speedY;
+      this.spriteX = this.collisionX - this.width * 0.5;
+      this.spriteY = this.collisionY - this.height * 0.5;
     }
   }
 
